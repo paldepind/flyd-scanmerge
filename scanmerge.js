a4a34058ee03de535bca2d888bf99fa6af9cbf75
paldepind <simonfv@gmail.com>
2015-04-26 08:56:01 +0200

    Update latest Flyd

--- scanmerge.js
+++ scanmerge.js
@@ -4,9 +4,9 @@ module.exports = flyd.curryN(2, function(pairs, acc) {
   var streams = pairs.map(function(p) { return p[0]; });
   var fns = pairs.map(function(p) { return p[1]; });
   return flyd.stream(streams, function(self, changed) {
-    if (changed !== undefined) {
-      var idx = streams.indexOf(changed);
-      acc = fns[idx](acc, changed());
+    if (changed.length > 0) {
+      var idx = streams.indexOf(changed[0]);
+      acc = fns[idx](acc, changed[0]());
     }
     return acc;
   }, true);
